b04c01413f092314f98ec748fc3463ea0a3ff60a
MarlonGun <marlonull30@gmail.com>
2026-04-11 19:29:33 -0500

    Correcion de la migracion

--- prisma/migrations/20260411191958_init_kuarzo_schema/migration.sql
+++ prisma/migrations/20260411191958_init_kuarzo_schema/migration.sql
@@ -4,8 +4,6 @@
   - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- DropTable
-DROP TABLE `user`;
 
 -- CreateTable
 CREATE TABLE `Rol` (
