248573392b22a3f146333c3c640623dd013c60cc
MarlonGun <marlonull30@gmail.com>
2026-06-22 14:51:02 -0500

    fix PrismaClient initialization in contacto.controller

--- src/controllers/contacto.controller.ts
+++ src/controllers/contacto.controller.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '../config/db';
 
 // Crear un nuevo contacto (Público)
 export const crearContacto = async (req: Request, res: Response) => {
